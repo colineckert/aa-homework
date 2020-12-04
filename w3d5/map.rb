095217186e00b93e41a96bfd00c180ce00aefd12
colineckert <colineckert10@gmail.com>
2020-12-03 23:25:01 -0800

    Create #set for Map class

---
+++ w3d5/map.rb
@@ -0,0 +1,31 @@
+class Map
+
+  attr_reader :map
+
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    pair_idx = nil
+
+    @map.each_with_index do |pair, idx|
+      pair_idx = idx if pair[0] == key 
+    end
+
+    if pair_idx.nil?
+      @map.push([key, value])
+    else
+      @map[pair_idx][1] = value
+    end
+  end
+
+  def get(key)
+    @map.each
+  end
+
+  def delete(key)
+
+  end
+
+end
