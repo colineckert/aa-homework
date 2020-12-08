abda8154cbd7c849cee2caf53f89255a190dff52
colineckert <colineckert10@gmail.com>
2020-12-07 23:54:57 -0800

    Finish Mancala assignment

--- w4d2/Mancala/lib/board.rb
+++ w4d2/Mancala/lib/board.rb
@@ -2,16 +2,19 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
     @cups = Array.new(14) { Array.new }
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
     @cups.each_with_index do |cup, i|
       if i != 6 && i != 13
         @cups[i] += [:stone, :stone, :stone, :stone]
       end
     end
-  end
-
-  def place_stones
-    # helper method to #initialize every non-store cup with four stones each
   end
 
   def valid_move?(start_pos)
@@ -20,12 +23,35 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
 
-    # call board.render at the end of each move
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -37,8 +63,16 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0...6].all?(&:empty?) || @cups[7...13].all?(&:empty?)
   end
 
   def winner
+    p1_score = @cups[6].count
+    p2_score = @cups[13].count
+    if p1_score == p2_score
+      :draw
+    else
+      p1_score > p2_score ? @name1 : @name2
+    end
   end
 end
