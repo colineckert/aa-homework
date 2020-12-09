0b33f3dc2818174cabfe21b94d068cc94b2fdb7e
colineckert <colineckert10@gmail.com>
2020-12-08 18:26:01 -0800

    Finish error handling homework

---
+++ w4d2/Error Handling Funtime/super_useful.rb
@@ -0,0 +1,72 @@
+require "byebug"
+# PHASE 2
+def convert_to_int(str)
+  str.to_i
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+class CoffeeError < StandardError
+  def message
+    "Please, no more caffeine. Try again."
+  end
+end
+
+class NotAFruitError < StandardError
+  def message
+    "That's not a fruit. Stay away!"
+  end
+end
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else 
+    raise NotAFruitError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit :)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotAFruitError => e
+    puts e.message
+  end
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    
+    raise ArgumentError.new('Only best friends can know each other for at least 5 years.') if yrs_known.to_i < 5
+    raise ArgumentError.new('Name cannot be blank') if name.length <= 0
+    raise ArgumentError.new('Favorite pastime cannot be blank') if name.length <= 0
+    
+    @name = name
+    @yrs_known = yrs_known.to_i
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
