ff2e97c87ef27417259c7de40e4ab9b05f55d721
colineckert <colineckert10@gmail.com>
2020-12-10 23:32:39 -0800

    Refactor methods

--- w4d5/octopus_problems.rb
+++ w4d5/octopus_problems.rb
@@ -76,7 +76,16 @@ p slow_dance("right-down", tiles_array)
 #> 3
 
 # Constant Dance!
-new_tiles_data_structure = { "up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3, "down" => 4, "left-down" => 5, "left" => 6,  "left-up" => 7 }
+new_tiles_data_structure = { 
+  "up" => 0, 
+  "right-up" => 1, 
+  "right" => 2, 
+  "right-down" => 3, 
+  "down" => 4, 
+  "left-down" => 5, 
+  "left" => 6,  
+  "left-up" => 7 
+}
 
 def fast_dance(dir, struct)
   struct[dir]
