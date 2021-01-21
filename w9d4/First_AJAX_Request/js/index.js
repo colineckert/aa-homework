3c366bf5a06eabdf7466a7b9a296586dda15bcd2
colineckert <colineckert10@gmail.com>
2021-01-21 00:38:00 -0800

    Complete AJAX weather homework

--- w9d4/First_AJAX_Request/js/index.js
+++ w9d4/First_AJAX_Request/js/index.js
@@ -1,5 +1,14 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b', 
+  type: 'GET',
+  success: function(weatherData) {
+    console.log(weatherData);
+  },
+  error: function() {
+    console.log("Error occurred");
+  }
+})
 
-// Add another console log here, outside your AJAX request
+console.log("AJAX test!")
