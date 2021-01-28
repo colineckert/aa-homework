d189b5f95554546f8f19a70db1e3ba0618fe4590
colineckert <colineckert10@gmail.com>
2021-01-27 23:44:04 -0800

    Complete Redux job listings homework

---
+++ w10d4/job_listing/frontend/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  city: "Please Select", 
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case "SWITCH_LOCATION":
+      return {
+        city: action.city,
+        jobs: action.jobs
+      };
+    default:
+      return state;
+  }
+};
+
+window.reducer = reducer;
+
+export default reducer;
