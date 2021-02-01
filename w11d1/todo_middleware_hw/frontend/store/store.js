2358c4f07da10f101d73086de331c6802e7d39f4
colineckert <colineckert10@gmail.com>
2021-01-31 19:21:37 -0800

    Complete middleware assignment

---
+++ w11d1/todo_middleware_hw/frontend/store/store.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const configureStore = (preloadedState = {}) => {
+
+  // Phase 3: Using Redux applyMiddleware
+  const store = createStore(
+    rootReducer, 
+    preloadedState, 
+    applyMiddleware(addLoggingToDispatch)
+    );
+
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+
+  return store;
+}
+
+export default configureStore;
